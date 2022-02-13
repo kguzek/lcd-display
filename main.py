6e7ed61cb7161c23c3f3e3dac831df6acf372ed1
Konrad (PC) <kolego199@gmail.com>
2022-02-13 01:14:07 +0100

    PyLint compliance

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
+"""A temperature controller for the Raspberry Pi made by Konrad Guzek."""
 
 # Standard library imports
-import random
 import signal
 import time
 
 # Third-party imports
-import Adafruit_DHT
-from corny_commons import file_manager, util
-from RPi import GPIO
-from RPLCD.gpio import CharLCD
+import Adafruit_DHT  # pylint: disable=import-error
+from corny_commons import file_manager
+from RPi import GPIO  # pylint: disable=import-error
+from RPLCD.gpio import CharLCD  # pylint: disable=import-error
 
 NUM_COLUMNS = 16
+# The GPIO pins used for transmitting data to the LCD
+LCD_PINS = [26, 19, 13, 6, 1, 7, 8, 25]
 
 # Initialise temperature sensor
 sensor = Adafruit_DHT.DHT22
@@ -35,32 +37,32 @@ def centred(text: str, num_columns: int = NUM_COLUMNS, span_entire_line: bool = 
     # The number of spaces that should be on either side of the string
     padding = (num_columns - len(text)) // 2
     # Make the string a total length of left space + original length
-    centred = text.rjust(padding + len(text))
+    _centred = text.rjust(padding + len(text))
     if span_entire_line:
         # Make the string the length of the entire line
-        centred = centred.ljust(num_columns)
-    # file_manager.log(f"Centred text: '{centred}'")
-    return centred
+        _centred = _centred.ljust(num_columns)
+    # file_manager.log(f"Centred text: '{_centred}'")
+    return _centred
 
 
-def type(fragment: str, interval: float = 0.1) -> None:
+def typewrite(fragment: str, interval: float = 0.1) -> None:
     """Types each character in the string one by one with the specified interval inbetween."""
     for char in fragment:
-        lcd.write_string(char)      
+        lcd.write_string(char)
         if char != " ":
             # Don't sleep if the current character is whitespace
-            time.sleep(interval)         
+            time.sleep(interval)
 
-    
+
 def intro() -> None:
     """Renders the intro text on the LCD."""
     file_manager.log("Playing intro...")
     lcd.cursor_mode = "blink"
-    type("Welcome!")
+    typewrite("Welcome!")
     time.sleep(0.25)
-    type(" Made by")
+    typewrite(" Made by")
     lcd.cursor_pos = (1, 0)  # Start of second line
-    type(centred("Konrad Guzek", span_entire_line = False))
+    typewrite(centred("Konrad Guzek", span_entire_line=False))
     time.sleep(0.5)
     lcd.cursor_mode = "hide"
 
@@ -68,10 +70,10 @@ def intro() -> None:
 def update_display_info() -> None:
     """Adds the humidity and temperature information to the second line of the LCD."""
     lcd.cursor_pos = (1, 0)
-    temp = random.randint(15, 40)
     humidity, temperature = Adafruit_DHT.read_retry(sensor, SENSOR_PIN)
     # 0x00 is the hex code for the LCD's custom defined character at location 0
-    lcd.write_string(centred(f"{humidity or -1:0.01f}%  {temperature or -1:0.01f}\x00C"))
+    lcd.write_string(
+        centred(f"{humidity or -1:0.01f}%  {temperature or -1:0.01f}\x00C"))
     time.sleep(2)
 
 
@@ -96,16 +98,23 @@ def main() -> None:
         # Clean up GPIO resources and clear the display
         lcd.close(clear=True)
 
+
 # When the user presses Ctrl+C (SIGIGN), the Python process interprets this as KeyboardInterrupt
 # This exception can be caught in the code above
 # When the user presses Ctrl+Z (SIGSTP), the Python process interprets this as having reached EOF
 # To make the program able to catch the user pressing Ctrl+Z, we remap it to raising the
 # KeyboardInterrupt exception.
-signal.signal(signal.SIGTSTP, lambda _signum, _stack_frame: signal.raise_signal(signal.SIGINT))
+
+def raise_keyboard_interrupt(_signum, _stackframe) -> None:
+    """Raises the signal.SIGINT signal which is interpreted as the KeyboardInterrupt exception."""
+    signal.raise_signal(signal.SIGINT)
+
+
+# signal.SIGSTP is a valid signal in unix-based systems; it's unsupported on Windows
+signal.signal(signal.SIGTSTP, raise_keyboard_interrupt) # pylint: disable=no-member
 
 # Initialise the global LCD object reference
 file_manager.log("Initialising LCD display...")
-lcd = CharLCD(pin_rs=21, pin_rw=20,  pin_e=16, pins_data=[26, 19, 13, 6, 1, 7, 8, 25], numbering_mode=GPIO.BCM, cols=16, rows=2)
-# lcd = CharLCD(pin_rs=40, pin_rw=38, pin_e=36, pins_data=[37, 35, 33, 31, 28, 26, 24, 22], cols=16, rows=2, numbering_mode=GPIO.BOARD)
+lcd = CharLCD(pin_rs=21, pin_rw=20,  pin_e=16, pins_data=LCD_PINS,
+              numbering_mode=GPIO.BCM, cols=16, rows=2)
 main()
-
