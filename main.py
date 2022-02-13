3e60a4067c427e5f468d91b5ec54dcd486c11b8c
Konrad (PC) <kolego199@gmail.com>
2022-02-13 03:30:59 +0100

    replace asyncio with threading

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 """A temperature controller for the Raspberry Pi made by Konrad Guzek."""
 
 # Standard library imports
-import asyncio
 import signal
+import time
+import threading
 
 # Third-party imports
 import Adafruit_DHT  # pylint: disable=import-error
@@ -44,56 +45,52 @@ def centred(text: str, span_entire_line: bool = True) -> str:
     return _centred
 
 
-async def typewrite(text: str, interval: float = 0.1) -> None:
+def typewrite(text: str, interval: float = 0.1) -> None:
     """Types each character in the string one by one with the specified interval inbetween."""
     for char in text:
         lcd.write_string(char)
         if char != " ":
             # Don't sleep if the current character is whitespace
-            await asyncio.sleep(interval)
+            time.sleep(interval)
 
 
-async def intro() -> None:
+def intro() -> None:
     """Renders the intro text on the LCD."""
     file_manager.log("Playing intro...")
     lcd.cursor_mode = "blink"
-    await typewrite("Welcome!")
-    await asyncio.sleep(0.25)
-    await typewrite(" Made by")
+    typewrite("Welcome!")
+    time.sleep(0.25)
+    typewrite(" Made by")
     lcd.cursor_pos = (1, 0)  # Start of second line
-    await typewrite(centred("Konrad Guzek", span_entire_line=False))
-    await asyncio.sleep(0.5)
+    typewrite(centred("Konrad Guzek", span_entire_line=False))
+    time.sleep(0.5)
     lcd.cursor_mode = "hide"
     lcd.clear()
     file_manager.log("Completed intro animation.")
 
 
-async def update_display_info() -> None:
+def update_display_info() -> None:
     """Adds the humidity and temperature information to the second line of the LCD."""
-    lcd.cursor_pos = (1, 0)
-    # Read the values from the GPIO-connected humidity and temperature sensor
-    # 22 is the type of the sensor; same as `Adafruit_DHT.DHT22`
-    result = await loop.run_in_executor(None, Adafruit_DHT.read_retry, 22, SENSOR_PIN)
-    humidity, temperature = result
-    # 0x00 is the hex code for the LCD's custom defined character at location 0
-    temp_details = f"{humidity or -1:0.01f}%  {temperature or -1:0.01f}\x00C"
-    lcd.write_string(centred(temp_details))
-    await asyncio.sleep(2)
-    await update_display_info()
+    while SHOULD_UPDATE_INFO:
+        lcd.cursor_pos = (1, 0)
+        # Read the values from the GPIO-connected humidity and temperature sensor
+        humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, SENSOR_PIN)
+        # 0x00 is the hex code for the LCD's custom defined character at location 0
+        temp_details = f"{humidity or -1:0.01f}%  {temperature or -1:0.01f}\x00C"
+        lcd.write_string(centred(temp_details))
+        time.sleep(2)
 
 
-async def main() -> None:
+def main() -> None:
     """Cleans up the LCD from previous usage and keeps the display updated."""
     lcd.clear()
     lcd.home()
     # Define custom character at location 0 with the above bitmap
     lcd.create_char(0, degree_sign)
     file_manager.log("LCD display initialised successfully!")
-    await intro()
-    loop.create_task(util.scroll_text(lcd, "Pi Temperature", max_num_scrolls=0))
-    loop.create_task(update_display_info())
-    # Wait until all tasks have completed
-    await asyncio.gather(*asyncio.all_tasks())
+    intro()
+    update_info_thread.start()
+    util.scroll(lcd, "Pi Temperature")
 
 
 # When the user presses Ctrl+C (SIGIGN), the Python process interprets this as KeyboardInterrupt
@@ -111,18 +108,23 @@ def raise_keyboard_interrupt(_signum, _stackframe) -> None:
 # signal.SIGTSTP is a valid signal in unix-based systems; it's unsupported on Windows
 signal.signal(signal.SIGTSTP, raise_keyboard_interrupt)  # pylint: disable=no-member
 
+SHOULD_UPDATE_INFO = True
+update_info_thread = threading.Thread(target=update_display_info)
+
 # Initialise the global LCD object reference
 file_manager.log("Initialising LCD display...")
 lcd = CharLCD(pin_rs=21, pin_rw=20,  pin_e=16, pins_data=LCD_PINS,
               numbering_mode=GPIO.BCM, cols=16, rows=2)
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
 try:
-    loop.run_until_complete(main())
+    main()
 except KeyboardInterrupt:
     # The user force exited the program
     print()  # Newline so log isn't on same line as user input
     file_manager.log("Quitting program (ctrl+c)")
 finally:
     # Clean up GPIO resources and clear the display
+    if update_info_thread.is_alive():
+        SHOULD_UPDATE_INFO = False
+        # Wait until the thread completes execution
+        update_info_thread.join()
     lcd.close(clear=True)
--- modules/util.py
+++ modules/util.py
@@ -1,41 +1,30 @@
 """General utility functions."""
 
 # Standard library imports
-import asyncio
+import time
 
 # Local application imports
 from . import NUM_COLUMNS
 
 
-async def scroll_text(lcd, text: str, row: int = 0, max_num_scrolls: int = 3,
-                      interval: float = 0.5) -> None:
+def scroll(lcd, text: str, row: int = 0, interval: float = 0.5, max_scrolls: int = None) -> None:
     """Renders the text on the LCD with a scrolling horizontal animation."""
     times_scrolled = 0
-
-    async def _scroll(stage: int = 1):
-        """Recursive function that progresses through the string's characters one by one."""
-        nonlocal times_scrolled
+    stage = 0
+    while max_scrolls is None or times_scrolled < max_scrolls:
         if stage > NUM_COLUMNS + len(text):
             stage = 1
             times_scrolled += 1
-        # Infinite max times scrolled if less than or equal to 0
-        if times_scrolled >= max_num_scrolls > 0:
-            return
         fragment = text.rjust(len(text) + NUM_COLUMNS)[stage:]
         lcd.cursor_pos = (row, 0)
         # Ensure string doesn't exceed the maximum length
         lcd.write_string(fragment[:NUM_COLUMNS].ljust(NUM_COLUMNS))
-        await asyncio.sleep(interval)
-        await _scroll(stage + 1)
-
-    await _scroll()
+        time.sleep(interval)
+        stage += 1
 
 
 if __name__ == "__main__":
     class LCD:  # pylint: disable=too-few-public-methods
         """Placeholder class for console demonstration."""
         write_string = print
-
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(scroll_text(LCD, "hello world", interval=0.1))
+    scroll(LCD, "hello world", interval=0.1, max_scrolls=3)
