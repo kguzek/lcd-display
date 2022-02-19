cb9a979afeaf32faf434432f876476891cd5d34f
Konrad (PC) <kolego199@gmail.com>
2022-02-19 02:34:24 +0100

    update GPIO pins

--- modules/__init__.py
+++ modules/__init__.py
@@ -4,4 +4,4 @@ from typing import Annotated
 
 NUM_ROWS: int = 2  # The number of lines of characters on the LCD
 NUM_COLUMNS: int = 16  # The number of characters on one line of the LCD
-SENSOR_PIN: int = 23  # The temperature & humidity sensor's GPIO pin number
+SENSOR_PIN: int = 8  # The temperature & humidity sensor's GPIO pin number
