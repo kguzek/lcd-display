01cafc62f141464799d4e1f57544ac0281fed16e
Konrad (PC) <kolego199@gmail.com>
2022-02-19 02:08:00 +0100

    add cleanup calls

--- main.py
+++ main.py
@@ -97,10 +97,12 @@ def main(lcd: CharLCD, adafruit) -> None:
         # Clean up GPIO resources and clear the display
         util.PROGRAM_IS_RUNNING = False
         file_manager.log("All processes terminated!")
+        lcd.cursor_mode = "none"
         lcd.close(clear=True)
         if util.ORIGINAL_SIGTSTP_HANDLER is not None:
             # The signal handler was modified in main()
             signal.signal(signal.SIGTSTP, util.ORIGINAL_SIGTSTP_HANDLER)  # pylint: disable=no-member
+        GPIO.cleanup()
 
 
 def instantiate_lcd() -> CharLCD:
